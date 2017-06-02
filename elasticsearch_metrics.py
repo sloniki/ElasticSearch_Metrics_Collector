fe0ca3cd6c55241214ffd62f4eb1230f86b62b9c
Yury Sadovenko <sadovenko@gmail.com>
2017-06-02 09:01:56 -0400

    Added argument parsing with argparse

--- elasticsearch_metrics.py
+++ elasticsearch_metrics.py
@@ -1,24 +1,13 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
+import argparse
+from elasticsearch import Elasticsearch
+import socket
+import sys
 
-# -*- coding: utf-8 -*-
-"""
+PORT = 9200
 
-__author__ = "Oleksandr Chyrkov"
-
-
-This script collects custom metrics from ES cluster.
-Metrics can be grouped by cluster or separately by nodes from cluster.
-
-Usage:
-
-    elasticsearch_metrics <option> <metric>
-
-Supported options:
-    - cluster
-    - node
-
+METRICS = """
 Supported metrics by options:
-
     Cluster:
         - active_primary_shards
         - active_shards
@@ -39,105 +28,69 @@ Supported metrics by options:
         - total_filter_cache_mem
         - total_field_data_mem
         - total_merges_mem
-
-
-Example:
-        Get cluster status:
-
-        $ elasticsearch_metrics cluster status
-
-        Get heap_used_in_bytes on current node:
-
-        $ elasticsearch_metrics node heap_used_in_bytes
-
 """
 
-# TODO: merges/segments count, file_descriptors count (implement more lovely script design)
+parser = argparse.ArgumentParser(description='Queries Elasticsearch for cluster/node internal metrics. Ver 1.1 beta')
+parser.add_argument('-H', metavar="<host>", default="127.0.0.1", help='Hostname or IP. Default - 127.0.0.1')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-n', help='Check node metric', metavar="<metric>")
+group.add_argument('-c', help='Check cluster metric', metavar="<metric>")
+group.add_argument('-l', help='List all metrics', action='store_true', default=False)
 
-from elasticsearch import Elasticsearch
-import sys
+args = parser.parse_args()
+if args.l:
+  print METRICS
+  sys.exit(0)
 
-
-# Try to establish a connection to elasticsearch.
 try:
-    es = Elasticsearch()
+    es = Elasticsearch([{'host': args.H, 'port': PORT}], sniff_on_start=True, sniffer_timeout=5)
 except Exception as e:
-    print 'Connection failed.'
+    print 'Connection to Elasticsearch failed!'
     sys.exit(1)
-
-# Print error message in case of unsupported  metric.
-def err_message(option, metric):
-
-    print "%s metric is not under support for %s option." % (metric, option)
-    sys.exit(1)
-
 
 def cluster_health(metric):
-
     result = es.cluster.health()
-
     print result[metric]
 
-
 def cluster_mem_stats(metric):
-
     result = es.cluster.stats()
     size = result['nodes']['jvm']['mem'][metric]
-
     print size
 
-
 def node_mem_stats(metric):
-
     node_stats = es.nodes.stats(node_id='_local', metric='jvm')
     node_id = node_stats['nodes'].keys()[0]
-
     if 'heap_used_percent' in metric:
-
         result = node_stats['nodes'][node_id]['jvm']['mem'][metric]
         print result
     else:
         if 'pool_young' in metric:
-
             result = node_stats['nodes'][node_id]['jvm']['mem']['pools']['young']
             size = result['used_in_bytes']
         elif 'pool_old' in metric:
-
             result = node_stats['nodes'][node_id]['jvm']['mem']['pools']['old']
             size = result['used_in_bytes']
         elif 'pool_survivor' in metric:
-
             result = node_stats['nodes'][node_id]['jvm']['mem']['pools']['survivor']
             size = result['used_in_bytes']
         else:
-
             result = node_stats['nodes'][node_id]['jvm']['mem']
             size = result[metric]
-
         print size
 
-
 def node_index_stats(metric):
-
     node_stats = es.nodes.stats(node_id='_local', metric='indices')
     node_id = node_stats['nodes'].keys()[0]
-
     if metric == 'total_merges_mem':
         result = node_stats['nodes'][node_id]['indices']['merges']
         size = result['total_size_in_bytes']
-
     if metric == 'total_filter_cache_mem':
         result = node_stats['nodes'][node_id]['indices']['filter_cache']
         size = result['memory_size_in_bytes']
-
     if metric == 'total_field_data_mem':
         result = node_stats['nodes'][node_id]['indices']['fielddata']
         size = result['memory_size_in_bytes']
-
     print size
-
-
-# Definition of checks
 
 cluster_checks = {'active_primary_shards': cluster_health,
                   'active_shards': cluster_health,
@@ -161,17 +114,11 @@ node_checks = {'heap_pool_young_gen_mem': node_mem_stats,
 
 
 if __name__ == '__main__':
-
-    if len(sys.argv) < 3:
-        print "Positional arguments count should be 3."
-        sys.exit(2)
-
     try:
-        if sys.argv[1] == 'cluster':
-            cluster_checks.get(sys.argv[2])(sys.argv[2])
-
-        if sys.argv[1] == 'node':
-            node_checks.get(sys.argv[2])(sys.argv[2])
-
+        if args.c:
+            cluster_checks.get(args.c)(args.c)
+        if args.n:
+            node_checks.get(args.n)(args.n)
     except TypeError:
-        err_message(sys.argv[1], sys.argv[2])
+        print("Wrong argument!")
+        sys.exit(1)
